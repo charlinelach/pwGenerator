5dcbd9cb3fdd0783c3c02b6e64ee3d3727800e1d
Charline <charlinerlach@gmail.com>
2020-09-24 00:50:07 -0500

    More .js changes

--- script.js
+++ script.js
@@ -7,26 +7,43 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lowLetter = alphabet.toLowerCase();
-var highLetter = alphabet.toUpperCase();
-
 function generatePassword() {
+  // Variables/arrays
+  // var password = "";
+  // var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // var uppercase = lowercase.toUpperCase();
+  // var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  // var symbol = ["~", "!", "@", "#", "$", "%", "&", "-", "+", "=", "_"];
 
   var length = prompt("How many password characters are you seeking?");
   
   // If they put a number out of the 8 to 128 Range.
   if ((length < 8) || (length > 128)) {
     alert("You may only have a password from 8 to 128 characters!");
+    return valid;
   }
 
-  var confirmLow = prompt("Do you want lowercase letters in it?");
-  var confirmHigh = prompt ("Do you want uppercase letters?");
+  // Character type confirming
+  var confirmLow = confirm("Do you want lowercase letters in it?");
+  var confirmHigh = confirm("Do you want uppercase letters?");
+  var confirmNumbers = confirm("Do you want numbers?");
+  var confirmSymbols = confirm("Do you want special characters?");
 
-  for (var pwBuild = 0; pwBuild < length; pwBuild++)
+  if ((confirmLow = false) && (confirmHigh = false) && (confirmNumbers = false) && (confirmSymbols = false)) {
+    alert("At least ONE character type must be selected.");
+    return valid;
+  }
+
+  //alert if no criteria selected
+  for (var pwBuild = 0; pwBuild < length; pwBuild++) {
+    //random number, then pull IF confirmed
+    //random letter, then pull IF confirmed
+
+  }
+
+  return password;
 }
 
 // Add event listener to generate button
